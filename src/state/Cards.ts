132abeaef905e5f77d91c68da5260411d65a53f3
Adrian Khashaki <Adrian.Khashaki@kolibrigames.com>
2024-01-27 20:12:22 +0100

    pushed broken cards

--- src/state/Cards.ts
+++ src/state/Cards.ts
@@ -12,117 +12,65 @@ export type Card = {
   type Cards = Record<string, Card>
 
 
-export const CARDS: Cards = {
-  card_0: {
-    name: "card_0",
-    action: "card_0",
-    type: "character",
-    cost: 1,
-    amount: 5
+export const CARDS: Cards = [
+  {
+    "Type": 0,
+    "Name": "Cat",
+    "Action": ""
   },
-  card_1: {
-    name: "card_1",
-    action: "card_1",
-    type: "character",
-    cost: 1,
-    amount: 5
+  {
+    "Type": 0,
+    "Name": "Dinosaur",
+    "Action": ""
   },
-  card_2: {
-    name: "card_2",
-    action: "card_2",
-    type: "character",
-    cost: 1,
-    amount: 5
+  {
+    "Type": 0,
+    "Name": "Gorilla",
+    "Action": ""
   },
-  card_3: {
-    name: "card_3",
-    action: "card_3",
-    type: "character",
-    cost: 1,
-    amount: 5
+  {
+    "Type": 0,
+    "Name": "Knight",
+    "Action": ""
   },
-  card_4: {
-    name: "card_4",
-    action: "card_4",
-    type: "character",
-    cost: 1,
-    amount: 5
+  {
+    "Type": 0,
+    "Name": "Pirate",
+    "Action": ""
   },
-  card_5: {
-    name: "card_5",
-    action: "card_5",
-    type: "character",
-    cost: 1,
-    amount: 5
+  {
+    "Type": 0,
+    "Name": "Wizard",
+    "Action": ""
   },
-  card_6: {
-    name: "card_6",
-    action: "card_6",
-    type: "character",
-    cost: 1,
-    amount: 5
+  {
+    "Type": 1,
+    "Name": "Bionic_Arm",
+    "Action": "wearing a "
   },
-  card_7: {
-    name: "card_7",
-    action: "card_7",
-    type: "character",
-    cost: 1,
-    amount: 5
+  {
+    "Type": 1,
+    "Name": "Force_Field_Shield",
+    "Action": "holding a "
   },
-  card_8: {
-    name: "card_8",
-    action: "card_8",
-    type: "character",
-    cost: 1,
-    amount: 5
+  {
+    "Type": 1,
+    "Name": "Frying_Pan",
+    "Action": "holding a "
   },
-  card_9: {
-    name: "card_9",
-    action: "card_9",
-    type: "component",
-    cost: 1,
-    amount: 5
+  {
+    "Type": 1,
+    "Name": "Jetpack",
+    "Action": "wearing a "
   },
-  card_10: {
-    name: "card_10",
-    action: "card_10",
-    type: "component",
-    cost: 1,
-    amount: 5
+  {
+    "Type": 1,
+    "Name": "Laser_Glasses",
+    "Action": "wearing "
   },
-  card_11: {
-    name: "card_11",
-    action: "card_11",
-    type: "component",
-    cost: 1,
-    amount: 5
-  },
-  card_12: {
-    name: "card_12",
-    action: "card_12",
-    type: "component",
-    cost: 1,
-    amount: 5
-  },
-  card_13: {
-    name: "card_13",
-    action: "card_13",
-    type: "component",
-    cost: 1,
-    amount: 5
-  },
-  card_14: {
-    name: "card_14",
-    action: "card_14",
-    type: "component",
-    cost: 1,
-    amount: 5
-  },
-  card_15: {
-    name: "card_15",
-    action: "card_15",
-    type: "component",
-    cost: 1,
-    amount: 5
+  {
+    "Type": 1,
+    "Name": "Magnetic_Glove",
+    "Action": "wearing a "
   }
-}
+]

594d5483f80e9890da3d36ab5980e9ad86d13b82
Yannick Wiggers <yannick.wiggers@kolibrigames.com>
2024-01-27 20:20:37 +0100

    Increases cards amount

--- src/state/Cards.ts
+++ src/state/Cards.ts
@@ -17,78 +17,78 @@ export const CARDS: Cards = {
     type: "character",
     name: "Cat",
     cost: 1,
-    amount: 1
+    amount: 5
   },
   Dinosaur: {
     type: "character",
     name: "Dinosaur",
     cost: 1,
-    amount: 1
+    amount: 5
   },
   Gorilla: {
     type: "character",
     name: "Gorilla",
     cost: 1,
-    amount: 1
+    amount: 5
   },
   Knight: {
     type: "character",
     name: "Knight",
     cost: 1,
-    amount: 1
+    amount: 5
   },
   Pirate: {
     type: "character",
     name: "Pirate",
     cost: 1,
-    amount: 1
+    amount: 5
   },
   Wizard: {
     type: "character",
     name: "Wizard",
     cost: 1,
-    amount: 1
+    amount: 5
   },
   Bionic_Arm: {
     type: "component",
     name: "Bionic_Arm",
     action: "wearing a ",
     cost: 1,
-    amount: 1,
+    amount: 5,
   },
   Force_Field_Shield: {
     type: "component",
     name: "Force_Field_Shield",
     action: "holding a ",
     cost: 1,
-    amount: 1,
+    amount: 5,
   },
   Frying_Pan: {
     type: "component",
     name: "Frying_Pan",
     action: "holding a ",
     cost: 1,
-    amount: 1,
+    amount: 5,
   },
   Jetpack: {
     type: "component",
     name: "Jetpack",
     action: "wearing a ",
     cost: 1,
-    amount: 1,
+    amount: 5,
   },
   Laser_Glasses: {
     type: "component",
     name: "Laser_Glasses",
     action: "wearing ",
     cost: 1,
-    amount: 1,
+    amount: 5,
   },
   Magnetic_Glove: {
     type: "component",
     name: "Magnetic_Glove",
     action: "wearing a ",
     cost: 1,
-    amount: 1,
+    amount: 5,
   }
 }

7a5904c2c5cdad2f28a0495a520a0c74e20e642e
Adrian Khashaki <Adrian.Khashaki@kolibrigames.com>
2024-01-27 17:13:20 +0100

    Added dalle api

--- src/schemas/prompt.ts
+++ src/schemas/prompt.ts
@@ -8,3 +8,8 @@ export const PromptResponse = z.object({
   gptResponse: z.string(),
   executionTimeMs: z.number()
 })
+
+export const ImageResponse = z.object({
+  dalleResponseUrl: z.string(),
+  executionTimeMs: z.number()
+})
